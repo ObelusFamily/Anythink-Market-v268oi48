31baae9d1adca6e71548c3e00842935bbbb8cbbb
Fumbani Banda <60394916+deadex-ng@users.noreply.github.com>
2022-12-23 15:37:38 +0000

    update script

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
-grep -i "@amazon.com" $1 | cut -d ',' -f 3,2 | tr ',' ' ' > $2
+#grep -i "@amazon.com" $1 | cut -d ',' -f 3,2 | tr ',' ' ' > $2
+grep -i "@amazon.com" $1 | cut -d ',' -f 3,2 | tr ',' ' ' |awk '{print $2, $1}' > $2
